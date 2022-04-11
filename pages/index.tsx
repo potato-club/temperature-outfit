1393cdbf9969fc4fe009da030fb2a39d9baabe08
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-04-11 20:08:53 +0900

    Merge branch 'main' of https://github.com/potato-club/temperature-outfit into front
    # Conflicts:
    #	components/common/index.tsx
    #	pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import { LayoutContainer, DivBox } from 'components/common';
 import type { NextPage } from 'next';
+import { TopWidgets } from 'components/TopWidgets';
 
 const Home: NextPage = () => {
   return (
     <LayoutContainer>
+<<<<<<< Updated upstream
       <DivBox width={500} height={100}>
         이렇게 사용하면 됨
       </DivBox>
+=======
+      <Header />
+      <DivBox width={500} height={100}></DivBox>
+>>>>>>> Stashed changes
     </LayoutContainer>
   );
 };
