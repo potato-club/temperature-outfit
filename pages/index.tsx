79f170bb1fece5c21eaafcd940572f81c56fef6e
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-04-27 20:39:20 +0900

    수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import { LayoutContainer, DivBox, Header } from 'components/common';
+import { DivBox, Header } from 'components/common';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <LayoutContainer>
+    <>
       <Header />
       <DivBox width={500} height={100}>
         이렇게 사용하면 됨
       </DivBox>
-    </LayoutContainer>
+      
+    </>
   );
 };
 
