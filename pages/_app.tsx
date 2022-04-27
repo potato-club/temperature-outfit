7ac6adb0b5457acb7f17665b9272d3f278673e6d
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-04-27 20:00:47 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { LayoutContainer } from 'components/common';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LayoutContainer>
+      <Component {...pageProps} />
+    </LayoutContainer>
+  );
 }
 
-export default MyApp
+export default MyApp;
