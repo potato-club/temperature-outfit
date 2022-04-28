43c953481f88e42b946127cf344d575e99a29a56
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-04-28 11:31:04 +0900

    Feat : 배경이미지 CSS 수정

--- components/common/LayoutContainer.tsx
+++ components/common/LayoutContainer.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import Image from 'next/image';
-import bluejacket from 'assets/img/background/fall/f1.jpg';
 
 export const LayoutContainer: React.FC = ({ children }) => {
   return <Container>{children}</Container>;
 };
 
 const Container = styled.div`
-  background-image: url('/winter/w11.jpg');
+  background-image: url('/winter/w3.jpg');
   background-repeat: no-repeat;
-  background-size: cover;
-  // 배경화면 알아보기
+  background-size: 100% 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
--- components/mainPage/clothesUI/ClothesMain.tsx
+++ components/mainPage/clothesUI/ClothesMain.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { LayoutContainer, DivBox, TypoGraphy } from 'components/common';
-import { EnrollButton, StarPrefer, Comment } from 'components/mainPage/clothesUI';
+import {
+  EnrollButton,
+  StarPrefer,
+  Comment,
+} from 'components/mainPage/clothesUI';
 import { color } from 'constants/color';
 import { AiOutlineRight, AiOutlineLeft } from 'react-icons/Ai';
 import { ClothesItem } from 'components/mainPage';
 
 export const ClothesMain: React.FC = () => {
   return (
-    <LayoutContainer>
-      <DivBox width={1400} height={488}>
-        <TypoGraphy
-          type={'Title'}
-          color={color.brandColor5}
-          fontWeight={'bold'}>
-          Today My Outfit
-        </TypoGraphy>
+    <DivBox width={1400} height={488}>
+      <TypoGraphy type={'Title'} color={color.brandColor5} fontWeight={'bold'}>
+        Today My Outfit
+      </TypoGraphy>
 
-        <StarPrefer />
+      <StarPrefer />
 
-        <ClothesContainer>
-          <AiOutlineLeft size={44} />
-          <ClothesItem />
-          <ClothesItem />
-          <ClothesItem />
-          <ClothesItem />
-          <AiOutlineRight size={44} />
-        </ClothesContainer>
+      <ClothesContainer>
+        <AiOutlineLeft size={44} />
+        <ClothesItem />
+        <ClothesItem />
+        <ClothesItem />
+        <ClothesItem />
+        <AiOutlineRight size={44} />
+      </ClothesContainer>
 
-        <Footer>
-          <Comment />
-          <EnrollButton />
-        </Footer>
-      </DivBox>
-    </LayoutContainer>
+      <Footer>
+        <Comment />
+        <EnrollButton />
+      </Footer>
+    </DivBox>
   );
 };
 
