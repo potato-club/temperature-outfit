1da7ea97d078141a0117bc1492590f6fd918d590
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-05-17 20:05:59 +0900

    Feat : 캘린더 페이지 생성하는 중에 프론트에서 오류 발견

---
+++ components/calendarPage/CalendarPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const CalendarPage = () => {
+  return <div>캘린더 페이지임</div>;
+};
+
+  
---
+++ pages/calendar.tsx
@@ -0,0 +1,12 @@
+import { CalendarPage } from 'components/calendarPage';
+import React from 'react';
+
+// const calendar = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// };
+
+export default CalendarPage;
