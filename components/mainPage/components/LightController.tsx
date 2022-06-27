575f0f68a805e9974b0635a3b7d277f8bcceb874
KoJaem <woalswhwh@naver.com>
2022-06-27 12:15:56 +0900

    feat : 조명 변경

--- components/mainPage/components/LightController.tsx
+++ components/mainPage/components/LightController.tsx
@@ -3,23 +3,12 @@ import React from "react";
 
 export const LightController = () => (
   <>
-    <directionalLight
-      castShadow
-      position={[0, 10, 0]}
-      intensity={4}
-      shadow-mapSize-width={1024}
-      shadow-mapSize-height={1024}
-      shadow-camera-far={50}
-      shadow-camera-left={-100}
-      shadow-camera-right={100}
-      shadow-camera-top={100}
-      shadow-camera-bottom={-100}
-    />
     <ambientLight intensity={0.4} />
-    <pointLight position={[-10, 0, -20]} intensity={0.5} />
-    <pointLight position={[0, -10, 0]} intensity={1.5} />
+    <hemisphereLight />
+    <spotLight position={[20, 20, 10]} intensity={4} />
   </>
 );
+
 
 // directionalLight : 태양광 이라고 생각하면 됨.
 
