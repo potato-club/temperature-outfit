a4569da2b212f59992cfba9992fd7feb377cd62a
KoJaem <woalswhwh@naver.com>
2022-07-01 23:42:11 +0900

    feat : recoil install 및 RecoilRoot 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { LayoutContainer, Header } from 'components/common';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LayoutContainer>
       <Header />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </LayoutContainer>
   );
 }
