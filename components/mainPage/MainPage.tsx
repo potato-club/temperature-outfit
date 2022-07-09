e70a4b977b01890414b8557fb076efeee4b17948
KoJaem <woalswhwh@naver.com>
2022-07-09 16:49:35 +0900

    feat : Props 로 정보 가져오게 구현

--- components/mainPage/MainPage.tsx
+++ components/mainPage/MainPage.tsx
@@ -1,10 +1,11 @@
 import styled from '@emotion/styled';
+import { mainDummy } from 'dummy/MainDummy';
 import React from 'react';
 import { MainCody, RegisterBtn, TodayInfo } from './components';
 export function MainPage() {
   return (
     <Container>
-      <TodayInfo />
+      <TodayInfo {...mainDummy}/>
       <MainCody />
       <RegisterBtn />
     </Container>
