e90a3cc45b3b0cbec524c70af413cb6546f618a9
sangkoon <bigyou00@gmail.com>
2022-07-12 11:31:47 +0900

    Feat - 로그아웃 연동 - user의 이름 recoil에 저장

--- components/mainPage/MainPage.tsx
+++ components/mainPage/MainPage.tsx
@@ -2,10 +2,12 @@ import styled from '@emotion/styled';
 import { mainDummy } from 'dummy/MainDummy';
 import React from 'react';
 import { MainCody, RegisterBtn, TodayInfo } from './components';
+
 export function MainPage() {
+
   return (
     <Container>
-      <TodayInfo {...mainDummy}/>
+      <TodayInfo {...mainDummy} />
       <MainCody />
       <RegisterBtn />
     </Container>
