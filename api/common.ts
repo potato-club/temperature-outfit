5eb66bd3741329944932d772cc8b86b75392b5f3
sanghoon <bigyou00@gmail.com>
2022-08-23 07:56:15 +0900

    Feat : [#74] oufit 변경, 삭제 API 생성

--- api/common.ts
+++ api/common.ts
@@ -5,12 +5,16 @@ import { setting } from 'constants/setting';
 export default {
   get: async (url: string) => await axios.get(setting.baseUrl + url),
 
-  getAll: async (url: string) =>
-    await axios.get(setting.baseUrl + url),
+  getAll: async (url: string) => await axios.get(setting.baseUrl + url),
 
   getWithParams: async (url: string, params: any) =>
     await axios.get(setting.baseUrl + url, params),
 
   post: async (url: string, params: any) =>
     await axios.post(setting.baseUrl + url, params),
+
+  delete: async (url: string) => await axios.delete(setting.baseUrl + url),
+
+  put: async (url: string, params: any) =>
+    await axios.put(setting.baseUrl + url, params),
 };
