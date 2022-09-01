df0fb677d34d5e52ee9a8e18a4c273657e6231e4
sanghoon <bigyou00@gmail.com>
2022-09-01 23:28:17 +0900

    [#74] 필터링 시킴 버그 발생 - 서버에 등록안됨 다른 브랜치에서도 그런지 확인해야함 - 렌더링이 2번씩되서 recoil에 값이 2개씩들어감 이건 개발모드라서 그럼 배포되면 괜찮다고 한다. - 수정하기에서 넘길 때와 그냥 등록할 때를 구분해야한다.

--- components/editPage/EditPage.tsx
+++ components/editPage/EditPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { TypoGraphy } from 'components/common';
 import { DressRoom, ReviewBox, Title } from './components';
@@ -6,20 +6,67 @@ import { categories } from 'types/editPage/categories';
 import { editDummy } from 'dummy/newEditDummy';
 import { ChooseModal } from 'components/modal';
 import { useRouter } from 'next/router';
+import {
+  topState,
+  bottomState,
+  etcState,
+  outerState,
+  shoesState,
+} from 'recoil/atom';
+import { useSetRecoilState } from 'recoil';
+import { clothesSubCategory, clothesMainCategory } from 'constants/index';
 
 export default function EditPage() {
   const [modalCategory, setModalCategory] = useState('');
   const router = useRouter();
-
   const dayQuery = router.query.day as string;
   const tempDay = '2222-22-22';
 
   const day = new Date(dayQuery ?? tempDay).toISOString().replace(/T.*$/, '');
 
-  const { imageUrl, rating, products, comment } = JSON.parse(
-    router.query.outfitData as string,
-  );
+  // 이게 있다면 useState로 관리해서 넘겨줘야할듯
+  // const { imageUrl, rating, products, comment } = JSON.parse(
+  //   router.query.outfitData as string,
+  // );
+  useEffect(() => {
+    router.query.outfitData;
+  }, []);
+  // 분류하기
+  console.log(categories);
+  const setTopValue = useSetRecoilState(topState);
+  const setBottomValue = useSetRecoilState(bottomState);
+  const setEtcValue = useSetRecoilState(etcState);
+  const setOuterValue = useSetRecoilState(outerState);
+  const setShoesValue = useSetRecoilState(shoesState);
 
+  const filterSubCategory = (category: string, categoryId: string) => {
+    return clothesSubCategory[category]
+      .map((item: any) => item.id)
+      .includes(categoryId);
+  };
+  const filterProduct = (product: any) => {
+    if (filterSubCategory('top', product.categoryId)) {
+      setTopValue((prev) => [...prev, product]);
+    }
+    if (filterSubCategory('outer', product.categoryId)) {
+      setOuterValue((prev) => [...prev, product]);
+    }
+    if (filterSubCategory('bottom', product.categoryId)) {
+      setBottomValue((prev) => [...prev, product]);
+    }
+    if (filterSubCategory('shoes', product.categoryId)) {
+      setShoesValue((prev) => [...prev, product]);
+    }
+    if (filterSubCategory('mainETC', product.categoryId)) {
+      setEtcValue((prev) => [...prev, product]);
+    }
+  };
+
+  // useEffect(() => {
+  //   products.forEach((product: any) => {
+  //     filterProduct(product);
+  //   });
+  // }, []);
   return (
     <Container>
       <Title
@@ -45,9 +92,9 @@ export default function EditPage() {
         </CodyBox>
         <ReviewBox
           day={day}
-          putImageUrl={imageUrl}
-          putRating={rating}
-          putComment={comment}
+          // putImageUrl={imageUrl}
+          // putRating={rating}
+          // putComment={comment}
         />
       </Contents>
       <ChooseModal categoryLabel={modalCategory} />
