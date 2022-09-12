044609e0bab41aab213599a54493d527ba422122
sanghoon <bigyou00@gmail.com>
2022-09-12 12:32:31 +0900

    style : head로 meta태그 viewport 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,15 @@ import type { AppProps } from 'next/app';
 import { LayoutContainer, Header } from 'components/common';
 import { RecoilRoot } from 'recoil';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>날씨별 옷차림</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <RecoilRoot>
         <LayoutContainer>
           <Header />
