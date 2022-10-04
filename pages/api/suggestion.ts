3d205b9b6b2cf92b91204bf8c3b96a71ec2b55c4
Pdom <ahb0327@naver.com>
2022-10-04 14:54:59 +0900

    Update : suggestion 타입 추가

--- pages/api/suggestion.ts
+++ pages/api/suggestion.ts
@@ -1,44 +1,32 @@
-import { Prisma } from '@prisma/client';
 import { prisma } from 'db';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { SuggestionGetRequest } from 'types';
-import { convertWeatherResponse } from 'utilities/api/converter';
+import {
+  SuggestionGetRequest,
+  SuggestionOutfit,
+  SuggestionResponse,
+} from 'types';
 import { updateWeather } from 'utilities/api/weather';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>,
+  res: NextApiResponse<SuggestionResponse>,
 ) {
   const query = req.query as SuggestionGetRequest;
 
-  if (!query.locationId) {
+  if (!query.temperature || typeof query.temperature != 'number') {
     // 필수 쿼리
-    return res.status(400).send('locationId 필요!');
+    return res.status(400);
   }
 
-  const location = await prisma.location.findUnique({
-    where: { id: +query.locationId },
-  });
+  //   const today = new Date();
 
-  if (!location) {
-    // 지역 없음
-    return res.status(400).send('locationId 오류!');
-  }
-
-  const today = new Date();
-
-  let result = await updateWeather(today, location, today);
-
-  const a: {
-    id: string;
-    imageUrl: string | null;
-    rating: number;
-    temperature: number;
-  }[] = await prisma.$queryRawUnsafe(
-    `SELECT a.id, a."imageUrl", a.rating FROM "Outfit" as a JOIN "Weather" as b ON (a.date = b.date AND a."locationId" = b."locationId") ORDER BY abs(${result.temperature} - b.temperature) LIMIT 5`,
+  const result: SuggestionOutfit[] = await prisma.$queryRawUnsafe(
+    `SELECT a.id, a."imageUrl", a.rating, b.temperature FROM "Outfit" as a JOIN "Weather" as b ON (a.date = b.date AND a."locationId" = b."locationId") ORDER BY abs(${Number(
+      query.temperature,
+    )} - b.temperature) LIMIT 5`,
   );
 
-  a.sort((a, b) => b.rating - a.rating);
+  result.sort((a, b) => b.rating - a.rating);
 
-  return res.json(a);
+  return res.json({ outfits: result });
 }
