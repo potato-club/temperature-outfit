7fa7d2ea2e7b750f8240698dd05d5727f74b6ee7
KoJaem <woalswhwh@naver.com>
2022-11-02 00:31:06 +0900

    feat : EditPage 코디등록 시간차 문제 해결 #112

--- src/constants/day.ts
+++ src/constants/day.ts
@@ -1,2 +1,7 @@
 export const today: Date = new Date();
 export const totalToday: string = today.toISOString().replace(/T.*$/, '');
+
+const offset = today.getTimezoneOffset() * 60000;
+export const koreaToday = new Date(today.getTime() - offset)
+  .toISOString()
+  .replace(/T.*$/, '');
