25f1356527e8f12ebeb85169b30718df714a53e3
sanghoon <bigyou00@gmail.com>
2022-11-08 22:10:59 +0900

    Feat [#107]

--- src/components/mainPage/components/RegisterBtn.tsx
+++ src/components/mainPage/components/RegisterBtn.tsx
@@ -3,17 +3,44 @@ import { TypoGraphy } from 'components/common';
 import { customColor, koreaToday } from 'constants/index';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { todayCodyApi } from 'api';
+import { confirmModal } from 'utils/interactionModal';
 
 export function RegisterBtn() {
   const router = useRouter();
+  const { refetch } = useQuery(
+    'haveTodayOutfit',
+    () => todayCodyApi.getManyOutfit(koreaToday, koreaToday),
+    {
+      enabled: false,
+      onSuccess: ({ data }) => {
+        if (data.length === 0) {
+          router.push({
+            pathname: `/edit`,
+            query: {
+              day: koreaToday,
+            },
+          });
+        } else if (data.length > 0) {
+          const { id } = data[0];
+          confirmModal(
+            '이미 등록된 코디가 있습니다.',
+            () => {
+              router.push({
+                pathname: `/outfitView/${id}`,
+              });
+            },
+            '이동',
+            '취소',
+          );
+        }
+      },
+    },
+  );
 
   const onClick = () => {
-    router.push({
-      pathname: `/edit`,
-      query: {
-        day: koreaToday,
-      },
-    });
+    refetch();
   };
 
   return (
