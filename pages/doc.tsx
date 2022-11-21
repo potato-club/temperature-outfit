2df986a440270a63e54aff77548fb4ac42ea6865
KoJaem <woalswhwh@naver.com>
2022-11-21 16:09:09 +0900

    style : doc 페이지 레이아웃 수정

--- pages/doc.tsx
+++ pages/doc.tsx
@@ -1,10 +1,21 @@
 import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
+import styled from '@emotion/styled';
 
 const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false });
 
 function Doc() {
-  return <SwaggerUI url="swagger.json" />;
+  return (
+    <Container>
+      <SwaggerUI url="swagger.json" />
+    </Container>
+  );
 }
 
 export default Doc;
+
+const Container = styled.section`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
