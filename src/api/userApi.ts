5958efe5224006ea5934a0cef67c5d8e9f88f185
sanghoon <bigyou00@gmail.com>
2022-11-22 19:46:52 +0900

    [#131] Feat
    - 유저 계정 삭제 API 생성

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,13 +1,16 @@
 import api from './common';
 
 export const userApi = {
-  getAllLocations:  () => {
+  getAllLocations: () => {
     return api.get('location');
   },
-  getUserLocation:  () => {
+  getUserLocation: () => {
     return api.get('user/location');
   },
-  changeUserLocation:  (data: any) => {
+  changeUserLocation: (data: any) => {
     return api.post('user/location', data);
+  },
+  deleteAuth: () => {
+    return api.delete('auth/delete');
   },
 };
