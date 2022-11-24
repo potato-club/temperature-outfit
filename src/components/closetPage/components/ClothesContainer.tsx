bf9a2c73fd4b2257cdefdb362f0783e83b8e57ed
KoJaem <woalswhwh@naver.com>
2022-11-24 20:57:39 +0900

    [#130] : Feat confirmModal text 내용 들어갈 수 있게 수정 closet 옷 삭제할때 주의메세지 추가

--- src/components/closetPage/components/ClothesContainer.tsx
+++ src/components/closetPage/components/ClothesContainer.tsx
@@ -20,7 +20,7 @@ export const ClothesContainer = () => {
   });
 
   const removeCheck = (id: string) => {
-    confirmModal('삭제 하시겠습니까?', () => mutate(id));
+    confirmModal('삭제 하시겠습니까?', () => mutate(id), '예', '아니오', '주의! 등록되어있는 코디에서 옷이 사라집니다');
     
   };
 
