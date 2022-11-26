d9868022cbd9d7427b7547bb1267a7f6008257eb
sanghoon <bigyou00@gmail.com>
2022-11-26 21:50:43 +0900

    Fixed - 날짜 data 하나로 통일

--- src/constants/day.ts
+++ src/constants/day.ts
@@ -1,7 +1,7 @@
 export const today: Date = new Date();
-export const totalToday: string = today.toISOString().replace(/T.*$/, '');
 
-const offset = today.getTimezoneOffset() * 60000;
-export const koreaToday = new Date(today.getTime() - offset)
-  .toISOString()
-  .replace(/T.*$/, '');
+export const koreaToday: string = today
+  .toLocaleDateString('ko')
+  .replaceAll('.', '-')
+  .replaceAll(' ', '')
+  .slice(0, -1);
