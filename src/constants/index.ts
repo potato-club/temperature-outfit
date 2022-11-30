4458c894d2e20d670be3e22d5301d88f76f7e667
sanghoon <bigyou00@gmail.com>
2022-11-30 22:18:43 +0900

    [#143] Chore - 달력 페이지 에러 핸들링

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@ export { customColor } from './customColor';
 export { clothesMainCategory } from './clothesMainCategory';
 export { clothesSubCategory } from './clothesSubCategory';
 export { setting } from './setting';
-export { koreaToday } from './day';
+export { koreaToday, today } from './day';
