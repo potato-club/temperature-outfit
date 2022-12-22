a73588ec138e94d74c5a23c9d2723a098b972e00
sanghoon <bigyou00@gmail.com>
2022-12-23 01:16:28 +0900

    [#153] Style - 로그인 페이지 디자인 수정 - 모서리에 돌아가는 사진들 애니메이션 추가

--- src/components/common/LeftSpin.tsx
+++ src/components/common/LeftSpin.tsx
@@ -5,30 +5,67 @@ import cloth1 from 'assets/img/cloth/1.jpg';
 import cloth2 from 'assets/img/cloth/2.jpg';
 import cloth3 from 'assets/img/cloth/3.jpg';
 
+interface InnerProps {
+  deg: number;
+}
 export const LeftSpin: React.FC = () => {
+  const temp = Array(8).fill(0);
+  const len = temp.length;
   return (
     <Wrapper>
-      <Circle></Circle>
+      {temp.map((_, i) => {
+        return (
+          <RightSection key={i}>
+            <Inner deg={45 * i}>
+              <Image width="400px" height="484px" alt="clothes" src={cloth2} />
+            </Inner>
+          </RightSection>
+        );
+      })}
+      {temp.map((_, i) => {
+        return (
+          <LeftSection key={i}>
+            <Inner deg={45 * i}>
+              <Image width="400px" height="484px" alt="clothes" src={cloth1} />
+            </Inner>
+          </LeftSection>
+        );
+      })}
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  position: absolute;
-  justify-content: center;
-  align-items: center;
-  left: 0;
-  transform: translate(-75%, 0);
-  overflow: hidden;
-`;
-const Circle = styled.div`
-  display: flex;
-  flex-direction: relative;
   width: 100vw;
-  height: 100vw;
-  border-radius: 50%;
-  border: 4px dotted #838282;
+  height: 100vh;
+  overflow: hidden;
+  position: absolute;
+`;
+
+const RightSection = styled.section`
+  top: -10vh;
+  left: 100vw;
+  transform: translate(-50%, -50%);
+  position: absolute;
   animation: spin 130s linear infinite;
+`;
+const LeftSection = styled.section`
+  top: 70vh;
+  right: 100vw;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  animation: spin 130s linear infinite;
+`;
+
+const Inner = styled.article<InnerProps>`
+  top: 50%;
+  left: 50%;
+  width: 16vw;
+  height: 40vh;
+  background: #63c4d1;
+  border-radius: 10px;
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.1);
+  opacity: 0.6;
+  transition: opacity 1s, transform 1s;
+  transform: ${(props) => `rotate(${props.deg}deg) translateY(-150%)`};
 `;
