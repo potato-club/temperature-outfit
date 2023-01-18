7669d10b56cd25beb1764e58f74162e9f29d7cd5
JoGeumJu <ccho8702@naver.com>
2023-01-18 23:02:51 +0900

    [#153] Style - TempSlide dummy데이터 삭제

--- src/components/mainPage/components/TempSlide.tsx
+++ src/components/mainPage/components/TempSlide.tsx
@@ -10,12 +10,6 @@ import { Suggestions } from 'types/mainPage';
 import { useRouter } from 'next/router';
 import { Rating } from 'react-simple-star-rating';
 
-import left1 from 'assets/img/loginSpin/left1.png';
-import left2 from 'assets/img/loginSpin/left2.jpg';
-import left3 from 'assets/img/loginSpin/left3.png';
-import left4 from 'assets/img/loginSpin/left4.png';
-import left5 from 'assets/img/loginSpin/left5.jpg';
-
 interface Props {
   suggestions: Suggestions[];
 }
@@ -26,38 +20,6 @@ interface ButtonStyle {
 const nullImage = '/codyDummy/Person_icon.png';
 
 export function TempSlide({ suggestions }: Props) {
-  const obj = [
-    {
-      id: 'q',
-      imageUrl: left1,
-      rating: 80,
-      temperature: '10.1',
-    },
-    {
-      id: 'q',
-      imageUrl: left2,
-      rating: 80,
-      temperature: '10.1',
-    },
-    // {
-    //   id: 'q',
-    //   imageUrl: left3,
-    //   rating: 80,
-    //   temperature: '10.1',
-    // },
-    // {
-    //   id: 'q',
-    //   imageUrl: left4,
-    //   rating: 80,
-    //   temperature: '10.1',
-    // },
-    // {
-    //   id: 'q',
-    //   imageUrl: left5,
-    //   rating: 80,
-    //   temperature: '10.1',
-    // },
-  ];
   const [isCurrent, setIsCurrent] = useState(3);
 
   const router = useRouter();
@@ -67,16 +29,20 @@ export function TempSlide({ suggestions }: Props) {
 
   SwiperCore.use([EffectCoverflow, Autoplay]);
   return (
-    <Container slideNum={obj.length}>
+    <Container slideNum={suggestions.length}>
       <StyledSwiper
-        slidesPerView={obj.length <= 3 ? obj.length : 0.5 * (obj.length - 1)}
-        loop={obj.length <= 3 ? false : true}
-        loopedSlides={obj.length <= 3 ? 0 : 5}
-        initialSlide={obj.length <= 3 ? 1 : 3}
+        slidesPerView={
+          suggestions.length <= 3
+            ? suggestions.length
+            : 0.5 * (suggestions.length - 1)
+        }
+        loop={suggestions.length <= 3 ? false : true}
+        loopedSlides={suggestions.length <= 3 ? 0 : 5}
+        initialSlide={suggestions.length <= 3 ? 1 : 3}
         centeredSlides
         spaceBetween={0}
         slideToClickedSlide
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
@@ -89,7 +55,7 @@ export function TempSlide({ suggestions }: Props) {
         }}
         effect={'coverflow'}
         modules={[EffectCoverflow]}>
-        {obj.map(({ id, imageUrl, rating, temperature }, idx) => {
+        {suggestions.map(({ id, imageUrl, rating, temperature }, idx) => {
           return (
             <StyledSwiperSlide key={id}>
               <ImageBox isCurrent={idx === isCurrent}>
@@ -138,9 +104,8 @@ const Container = styled.section<ButtonStyle>`
   max-width: calc(30% + 14% * ${(props) => props.slideNum});
   height: 100%;
   padding: 10% calc(23% - 4px);
-  padding: ${(props) => props.slideNum <= 3 && '0'};
+  padding: ${(props) => props.slideNum && props.slideNum <= 3 && '0'};
   overflow: hidden;
-  overflow: ${(props) => props.slideNum <= 2 && 'visible'};
 `;
 
 const StyledSwiper = styled(Swiper)`
