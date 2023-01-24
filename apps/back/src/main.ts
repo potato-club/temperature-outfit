2b52eea139449d761e2d6b392a1ec625f934690c
Pdom <pdom0327@gmail.com>
2023-01-24 21:56:15 +0900

    Update cors

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -12,6 +12,7 @@ import { PrismaService } from './prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
+    cors: true,
   });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
