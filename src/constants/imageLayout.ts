e52f4885ba714f51697c17d0cd1475241a398be5
JoGeumJu <ccho8702@naver.com>
2023-01-26 17:15:06 +0900

    [#153] Style - 이미지 크기 정의값 변경 - middleSquare 값 추가

--- src/constants/imageLayout.ts
+++ src/constants/imageLayout.ts
@@ -2,10 +2,12 @@ interface ImgType {
   square: string;
   width: string;
   height: string;
+  middleSquare: string;
 }
 const imageLayout: ImgType = {
-  square: '136',
+  square: '148',
   width: '150',
   height: '300',
+  middleSquare: '168',
 };
 export default imageLayout;
