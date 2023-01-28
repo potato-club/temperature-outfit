c85029dfa133d6289a47ee6c9cd4531626b05db6
sanghoon <bigyou00@gmail.com>
2023-01-28 21:11:00 +0900

    Refactoring - 양식 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
 import { LoginUi } from 'components/loginPage';
-// import { MainPage } from 'components/mainPage/MainPage';
-import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return <LoginUi />;
-};
 
-export default Home;
+export default LoginUi;
