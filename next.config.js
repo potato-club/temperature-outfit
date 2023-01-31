c29eb95364fb9736ea0e0d6a8852eb4cc00ea127
KoJaem <woalswhwh@naver.com>
2023-01-31 22:25:30 +0900

    Deploy feat : 배포 환경 설정

--- next.config.js
+++ next.config.js
@@ -13,5 +13,7 @@ module.exports = withTM({
   images: {
     domains: ['almurgynzlwuwereyhzx.supabase.co'],
     // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+    loader: 'akamai',
+    path: '',
   },
 });
